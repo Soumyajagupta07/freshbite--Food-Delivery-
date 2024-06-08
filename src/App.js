64abe4331049127f2d65a449ebd05e79467dce08
Soumyajagupta07 <guptasoumyaja@gmail.com>
2024-06-08 14:55:07 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <div className='logo-container'>
+        <img className="logo" src="https://w7.pngwing.com/pngs/894/279/png-transparent-online-food-ordering-food-delivery-grubhub-others-food-service-logo-thumbnail.png" alt="logo"/>
+        <h1 className='fresh'>FreshBite</h1>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+const RestroCard = () => {
+  return(
+    <div className='res-card'>
+      <h3> Biryani </h3>
+      </div>
+  )
+}
+const Body = () => {
+  return(
+    <div>
+      <div className='Search'>Search</div>
+      <div className='res-container'>
+        <RestroCard/>
+      </div>
+    </div>
+  )
+}
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body/>
+    </div>
+  );
+}
+export default AppLayout;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
