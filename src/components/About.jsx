af9061456ed44a9583b28fa5e824b4133150caf5
Soumyajagupta07 <guptasoumyaja@gmail.com>
2024-08-22 19:57:32 +0530

    committed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,50 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-    return(
-        <div>
-            <h1>About</h1>
-            <p>It is about page</p>
-            <User/>
-            <UserClass name={"Soumya Gupta"} location={"Kolkata"}/>
-        </div>
-    )
+class About extends Component{
+    constructor(props){
+        super(props);
+        console.log("Parent Constructor");
+    }
+
+    componentDidMount(){
+        console.log("Parent Component Didmount");
+    }
+
+    render(){
+        console.log("Parent render");
+        return(
+            <div>
+                <h1>About Class Component</h1>
+                <h2>This is React learning</h2>
+                <UserClass name={"Soumya Gupta"} location={"Kolkata"}/>
+            </div>
+        )
+    }
 }
+
+/*
+- Parent Constructor
+- Parent Render
+  - Soumyaja Constructor
+  - Soumyaja Render
+  - Soumyaja ComponentDidMount
+
+  - Jinia Constructor
+  - Jinia Render
+  - Jinia ComponentDidMount
+*/
+
+// const About = () => {
+//     return(
+//         <div>
+//             <h1>About</h1>
+//             <p>It is about page</p>
+//             <User/>
+//             <UserClass name={"Soumyaja Gupta"} location={"Kolkata"}/>
+//             <UserClass name={"Jinia Gupta"} location={"India"}/>
+//         </div>
+//     )
+// }
 export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -3,10 +3,24 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);     //always for class components
+
+        this.state = {
+            count: 0,
+            count2: 2,
+        };
     }
     render() {
+        const {name, location} = this.props;
+        const {count} = this.state;
         return (
             <div className="user_card">
+                <h1>Count: {count}</h1>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count + 1,
+                        count2: this.state.count2 + 1,
+                    })
+                }}></button>
             <h2>Name: {this.props.name}</h2>      
             <h3>Location: </h3>
             <h3>Contact: </h3>
